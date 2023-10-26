f9840a5336e5afd8c602ac7d00c6d8a691bcd732
Tushar_saini <148748033+tusharsaini1508@users.noreply.github.com>
2023-10-26 11:35:03 +0530

    Add files via upload
    marry calculator

---
+++ marry calculator.py
@@ -0,0 +1,18 @@
+
+age = int(input("Entr your age: "))
+if(age >= 18):
+    print("now you are an adult:")
+    if(age >=21):
+     print("you can marry now")
+
+  
+        
+    gender=str(input("enter your gender m or f:"))
+    if(gender == "f"):
+      print("you can marry now")
+    else:
+      print("you can not marry now")
+      
+
+else:
+  print("you are not an adult")
