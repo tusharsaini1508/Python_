0f05436f197d7cdde5d9489eb20d5db8c65bb6ef
Tushar_saini <148748033+tusharsaini1508@users.noreply.github.com>
2023-10-26 12:55:02 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -2,18 +2,20 @@ x=float(input("enter teh value of first number"))
 y=float(input("enter the value of second number"))
 calculation=input("enter the choice +,-,*,/")
 if calculation=="+":
-  print(x+y)
+  print(f"The addition of x and y is = {x+y}")
 
 elif calculation=="-":
-  print(x-y)
+  print(f"The Subtraction of x and y is ={x-y} ")
 
 elif calculation=="*":
-  print(x*y)
+  print(f"The multiplication of x and y is = {x*y}")
 
 elif calculation=="/":
-  print(x/y)
 
-if y==0:
-  print("division by zero not allowed")
-else:
-  print(x/y)
+  if y==0:
+    print("division by zero not allowed")
+    if x==0:
+      print("division by zero is not allowed")
+  else:
+    print(f"the division of x and y is {x/y}")
+
