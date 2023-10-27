44b0faeb8d4adc02a982dd5e4b46ffa62b1b5000
Tushar_saini <148748033+tusharsaini1508@users.noreply.github.com>
2023-10-27 10:59:35 +0530

    marry calculator.py

--- marry calculator.py
+++ marry calculator.py
@@ -1,18 +1,26 @@
-
-age = int(input("Entr your age: "))
-if(age >= 18):
-    print("now you are an adult:")
-    if(age >=21):
-     print("you can marry now")
+gender=input("enter your gender (m/f): ")
+if gender=="m":
+    print("you are male")
+  
+elif gender=="f":
+  print("you are female")
 
   
-        
-    gender=str(input("enter your gender m or f:"))
-    if(gender == "f"):
-      print("you can marry now")
-    else:
-      print("you can not marry now")
-      
+age=int(input("Enter your age"))
+if age>=18:
+  print(f"you are an adult your age =:{age}")
 
 else:
-  print("you are not an adult")
+  print(f"you are not an adult your age is {age}")
+
+if gender=="m":
+  if age>=21:
+    print(f"you can marry now your age is {age}")
+  else:(f"you can't marry now your age is {age}")
+    
+
+elif gender=="f":
+  if age>=18:
+    print(f"you can marry now your age is =:{age}")
+  else:(f"you can't marry now your age is =:{age}")
+    
