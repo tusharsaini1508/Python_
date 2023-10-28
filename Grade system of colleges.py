e924fc1a7ec2c02b457a6c6b892409a41879fc63
Tushar_saini <148748033+tusharsaini1508@users.noreply.github.com>
2023-10-28 20:31:08 +0530

    colleges grading system

---
+++ Grade system of colleges.py
@@ -0,0 +1,46 @@
+name=input("Enter your name =:")
+number=int(input("Enter your number =:"))
+if number>=90and number<=100:
+  print(f"hii {name} your grade is ,O,= Outstanding")
+elif number>=80and number<=89:
+    print(f"hii {name} your grade is ,D+,= Excellent")
+elif number>=75and number<=79:
+      print(f"hii {name} your grade is ,D,= Destinction")
+elif number>=70and number<=74:
+        print(f"hii {name} your grade is ,A+,= Very Good")
+elif number>=60and number<=69:
+          print(f"hii {name} your grade is ,A,= Good")
+elif number>=50and number<=59:
+            print(f"hii {name} your grade is ,B,= Average")
+elif number>=40and number<=49:
+              print(f"hii {name} your grade is ,C,= Satisfactory")
+elif number>=00and number<=39:
+                print(f"hii {name} your grade is ,U,= Re-Appear")
+elif number>=00and number<=00:
+                  print(f"hii {name} your grade is ,AAA,=ABSENT")
+
+  
+
+   
+    
+                            
+
+  
+# elif choice==2:
+# grade=input("Enter your grade")
+# if grade==O:
+#   print(f"hii {name} outstanding your numbers are between 90-100 ")
+#   if grade==D+:
+#     print(f"hii {name} excellent your numbers are between 80-89")
+#     if grade==D:
+#       print(f"hii{name} Destinction your numbers are between 75-79"")
+#             if grade==A+:
+#               print(f"hii {name} Very Good your numbers are between 70-74")
+#               if grade==A:
+#                 print(f"hii {name} Good your numbers are between 60-69")
+#                 if grade==B:
+#                   print(f"hii {name} Average your numbers are between 50-59")
+#                   if grade==C:
+#                     print(f"hii {name} Satisfactory your numbers are between 40-49")
+#                     if grade==U:
+#                       print(f"hii {name} Re-Appear your numbers are between 0-39")
