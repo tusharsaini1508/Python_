471bd4ee8b26a47193c9a17cf08fbc47765ff0e1
Tushar_saini <148748033+tusharsaini1508@users.noreply.github.com>
2023-11-21 12:35:40 +0530

    bill calculator for a cafe.py

--- bill calculator for a cafe.py
+++ bill calculator for a cafe.py
@@ -2,7 +2,10 @@ name=input("Enter your name:= ")
 print(f"hii {name} Welcome in bill calculator")
 
 phone_number=input("Enter your phone number")
-print(f"we will send you notification on your number{phone_number}")
+print(f"we will send you notification on your number =: {phone_number}")
+
+address=input("Enter your address")
+print(f"we can also deliver the food on your address =: {address}")
 
 Bill=int(input("Enter your bill in $"))
 print(f"Your bill is {Bill}$")
@@ -18,3 +21,5 @@ split=int(input("Enter how many person want to share the bill"))
 Total_bill_per_person=Total_bill/split
 round_Bill=round(Total_bill_per_person,2)
 print(f"hii {name} you should pay the bill of {round_Bill}$")
+
+print(f"{name} Thank you for visiting our cafe see you again")
